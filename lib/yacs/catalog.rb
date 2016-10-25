3fa40806dc95ba7945bff9b6199c2b29fa6a832f
Richie <richiejoeyoung@gmail.com>
2016-10-25 00:14:35 -0400

    generate gem

---
+++ lib/yacs/catalog.rb
@@ -0,0 +1,7 @@
+require "yacs/catalog/version"
+
+module Yacs
+  module Catalog
+    # Your code goes here...
+  end
+end
---
+++ lib/yacs/catalog/version.rb
@@ -0,0 +1,5 @@
+module Yacs
+  module Catalog
+    VERSION = "0.1.0"
+  end
+end
